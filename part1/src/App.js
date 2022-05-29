b40374d6be8f041e0742891d8aa90e8d951f173c
chipsandtea <hsiao.christopher@gmail.com>
2022-05-29 16:29:42 -0700

    Exercises 1.2

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const Header = (prop) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>{prop.name}</h1>
+    </>
+  )
 }
 
-export default App;
+const Part = (prop) => {
+  if (prop.course== null) return;
+  const {name, count} = prop.course;
+  return (
+    <p>
+      {name} {count}
+    </p>
+  )
+}
+
+const Content = (prop) => {
+  return (
+    <div>
+      {prop.courses.map((course) => {
+        return (
+          <Part course={course} key={course.key}/>
+        );
+      })}
+    </div>
+  )
+}
+
+const Total = (prop) => {
+  let total = 0;
+  if (prop.courses != null) {
+    total = prop.courses.reduce((sum, course) => {
+      sum += course.count;
+      return sum;
+    }, 0);
+  }
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const courses = [
+    {key: 'p1', name: part1, count: exercises1},
+    {key: 'p2', name: part2, count: exercises2},
+    {key: 'p3', name: part3, count: exercises3}
+  ];
+
+  return (
+    <div>
+      <Header name={course}/>
+      <Content courses={courses}/>
+      <Total courses={courses}/>
+    </div>
+  )
+}
+
+export default App
