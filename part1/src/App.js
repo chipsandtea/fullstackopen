e1f60bd6cc5145933e67376d84aa413019ea81be
chipsandtea <hsiao.christopher@gmail.com>
2022-05-29 17:38:56 -0700

    [React State] - code from react state section.

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,68 +1,36 @@
+import { useState } from 'react'
 
-const Header = (prop) => {
-  return (
-    <>
-      <h1>{prop.name}</h1>
-    </>
-  )
-}
+const Display = ({counter}) => <div>{counter}</div>;
+ 
 
-const Part = (prop) => {
-  if (prop.course== null) return;
-  const {name, count} = prop.course;
-  return (
-    <p>
-      {name} {count}
-    </p>
+const Button = ({text, onClick}) => (
+    <button onClick={onClick}>
+      {text}
+    </button>
   )
-}
-
-const Content = (prop) => {
-  return (
-    <div>
-      {prop.courses.map((course) => {
-        return (
-          <Part course={course} key={course.key}/>
-        );
-      })}
-    </div>
-  )
-}
-
-const Total = (prop) => {
-  let total = 0;
-  if (prop.courses != null) {
-    total = prop.courses.reduce((sum, course) => {
-      sum += course.count;
-      return sum;
-    }, 0);
-  }
-  return (
-    <p>Number of exercises {total}</p>
-  )
-}
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const [ counter, setCounter ] = useState(0)
 
-  const courses = [
-    {key: 'p1', name: part1, count: exercises1},
-    {key: 'p2', name: part2, count: exercises2},
-    {key: 'p3', name: part3, count: exercises3}
-  ];
+  const increment = () => {
+    setCounter(counter + 1);
+  }
+
+  const decrement = () => {
+    setCounter(counter -1);
+  }
+
+  const reset = () => {
+    setCounter(0);
+  }
 
   return (
-    <div>
-      <Header name={course}/>
-      <Content courses={courses}/>
-      <Total courses={courses}/>
-    </div>
+    <>
+      <Display counter={counter}/>
+      <Button text='plus' onClick={increment}/>
+      <Button text='minus' onClick={decrement}/>
+      <Button text='reset' onClick={reset}/>
+    </>
   )
 }
 
